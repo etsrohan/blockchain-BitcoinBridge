a1aacf397a9b76ae67fd0596604baf69e36619d2
NexgRohan <88819424+NexgRohan@users.noreply.github.com>
2021-12-14 17:44:20 +0530

    Add files via upload

--- Base/Contracts/SupplyChain.sol
+++ Base/Contracts/SupplyChain.sol
@@ -6,7 +6,7 @@ contract ClothingSupplyChain{
     enum MaterialType {Unknown, Cotton, Ethylene}
     enum MaterialState {NotCreated, Enroute, Delivered, Failed}
     enum BatchType {Unknown, Cotton, Polyester}
-    enum BatchState {NotCreated, Created, Shipped}
+    enum BatchState {NotCreated, Processing, Complete, Shipped}
     enum ItemType {Unknown, TShirt, Shirt, Pants}
     enum SaleState {NotSold, Sold, Returned}
 
@@ -26,10 +26,11 @@ contract ClothingSupplyChain{
     // of manufactured clothes.
     struct ManufactureBatch {
         BatchType fabric_type;
+        ItemType item_type;
         BatchState state;
         uint256 manufacture_date;
         uint256 machine_id;
-        uint256 num_clothes;
+        uint256 num_items;
         uint256 price_per;          // price in cents per clothing made
         uint256 cost_per;           // cost in cents per clothing made
     }
@@ -37,19 +38,21 @@ contract ClothingSupplyChain{
     // Struct to represent an individual item
     struct Item {
         SaleState state;
-        ItemType item_type;
         uint256 receipt_number;
         uint256 retail_price;
         uint256 date_of_sale;
-        MaterialDelivery material;
         ManufactureBatch manufacturing;
     }
 
     // EVENTS
-    event NewDeliveryCreated(uint256 indexed delivery_id, address employee_address, string supplier);
+    event NewDeliveryCreated(uint256 indexed delivery_id, address employee_address, string supplier, MaterialType material);
     event DeliveryCreationFailed(string supplier, uint8 material, uint256 weight, uint256 cost, string message);
-    event DeliveryReceived (uint256 indexed delivery_id);
-    event DeliveryCanceled (uint256 indexed delivery_id);
+    event NewBatchCreated(uint256 indexed batch_id, address employee_address, BatchType fabric, ItemType apparel);
+    event BatchCreationFailed(uint8 fabric, uint256 machine_id, string message);
+    event BatchCompletionFailed(uint256 indexed batch_id, string message);
+    event BatchCompleted(uint256 indexed batch_id, uint256 num_items);
+    event DeliveryReceived(uint256 indexed delivery_id);
+    event DeliveryCanceled(uint256 indexed delivery_id);
     event NewItemCreated(uint256 indexed item_id, uint8 item_type);
 
     // MODIFIERS
@@ -58,9 +61,15 @@ contract ClothingSupplyChain{
         _;
     }
 
+    modifier batch_processing (uint256 batch_id){
+        require (batches[batch_id].state == BatchState.Processing);
+        _;
+    }
+
     // VARIABLES
     address admin;
-    uint256 [2] private material_weights;
+    uint256 [2] private material_weights;   // amount of material available; 0 -> Cotton, 1 -> Ethylene
+    uint256 [6] private num_apparel;        // 0 -> cotton tshirt, 1 -> polyester tshirt, 2 -> cotton shirt, 3 -> polyester shirt, 4 -> cotton pants, 5 -> polyester pants
     uint256 private num_deliveries;
     uint256 private num_batches;
     uint256 private num_items;
@@ -75,6 +84,7 @@ contract ClothingSupplyChain{
         admin = msg.sender;
     }
 
+    // --------------------------Supplier--------------------------
     // A function to create a delivery order for new materials
     function create_delivery_order(
         /**Material Details**/
@@ -113,7 +123,7 @@ contract ClothingSupplyChain{
         deliveries[delivery_id].weight = weight;
         deliveries[delivery_id].cost = cost;
 
-        emit NewDeliveryCreated(delivery_id, msg.sender, supplier_name);
+        emit NewDeliveryCreated(delivery_id, msg.sender, supplier_name, deliveries[delivery_id].mat_type);
         return true;
     }
 
@@ -147,14 +157,94 @@ contract ClothingSupplyChain{
         return true;
     }
 
+    
+    // --------------------------Manufacture--------------------------
     // A function to start processing a new batch of clothes being proccessed
-    function start_manufacturing(
-        /**Manufacturing Details**/
-
+    function start_batch(
+        /**Batch Details**/
+        uint8 fabric,
+        uint8 apparel,
+        uint256 machine_id
     )
         public
         returns (bool)
     {
-        
+        uint256 batch_id = num_batches++;
+
+        // Put correct fabric type in order
+        // 0 -> Cotton, 1 -> Ethylene
+        if (fabric == 0) batches[batch_id].fabric_type = BatchType.Cotton;
+        else if (fabric == 1) batches[batch_id].fabric_type = BatchType.Polyester;
+        else {
+            num_batches--;
+            emit BatchCreationFailed(
+                fabric,
+                machine_id,
+                "Incorrect Fabric Type"
+            );
+            return false;
+        }
+
+        // Put correct apparel type in order
+        // 0 -> T-Shirt, 1 -> Shirt, 2 -> Pants
+        if (apparel == 0) batches[batch_id].item_type = ItemType.TShirt;
+        else if (apparel == 1) batches[batch_id].item_type = ItemType.Shirt;
+        else if (apparel == 2) batches[batch_id].item_type = ItemType.Pants;
+        else {
+            num_batches--;
+            emit BatchCreationFailed(
+                fabric,
+                machine_id,
+                "Incorrect Apparel Type"
+            );
+            return false;
+        }
+
+        batches[batch_id].machine_id = machine_id;
+        batches[batch_id].state = BatchState.Processing;
+        emit NewBatchCreated(batch_id, msg.sender, batches[batch_id].fabric_type, batches[batch_id].item_type);
+        return true;
     }
+
+    // A function to end manufacture of an apparel
+    // Assume 1 item requires 250g of material
+    function complete_batch(
+        /**Batch Details**/
+        uint256 batch_id,
+        uint256 batch_output,
+        uint256 price,
+        uint256 cost
+    )
+        public
+        batch_processing(batch_id)
+        returns (bool)
+    {
+        require (price > 0, "Price cannot be zero");
+        require (cost > 0, "Cost cannot be zero");
+
+        if (batches[batch_id].fabric_type == BatchType.Cotton){
+            require (material_weights[0] > batch_output * 250);
+            material_weights[0] -= batch_output * 250;
+        } else if (batches[batch_id].fabric_type == BatchType.Polyester){
+            require (material_weights[1] > batch_output * 250);
+            material_weights[1] -= batch_output * 250;
+        } else {
+            emit BatchCompletionFailed(batch_id, "Batch not Initiated");
+            return false;
+        }
+        
+        batches[batch_id].state = BatchState.Complete;
+        batches[batch_id].manufacture_date = block.timestamp;
+        batches[batch_id].num_items = batch_output;
+        batches[batch_id].price_per = price;
+        batches[batch_id].cost_per = cost;
+
+        // Add the appropriate number of apparel to the num_apparel array
+        
+        // Create Items with the tagged details
+
+        emit BatchCompleted (batch_id, batches[batch_id].num_items);
+        return true;
+    }
+
 }
